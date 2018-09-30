f5ba9651191ea28e2b742e1a6a1a792577ca875d
Raphael Tiersch <tiersch.raphael@gmail.com>
2018-09-30 20:08:02 +0200

    finished the linux environment wrapper. Mac still to go.

--- pkg/config_xdg.go
+++ pkg/config_xdg.go
@@ -1,4 +1,4 @@
-// +build !windows,!darwin
+//+build !windows,!darwin
 
 package pkg
 
@@ -10,72 +10,88 @@ import (
 // https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html
 
 const (
-	CONFIG_LOCAL  = "XDG_CONFIG_HOME"
-	CONFIG_SHARED = "XDG_CONFIG_DIRS"
-	CACHE         = "XDG_CACHE_HOME"
-	DATA_LOCAL    = "XDG_DATA_HOME"
-	DATA_SHARED   = "XDG_DATA_DIRS"
+	envHome              = "HOME"
+	envUserConfig        = "XDG_CONFIG_HOME"
+	envGlobalConfig      = "XDG_CONFIG_DIRS"
+	envUserTemp          = "XDG_CACHE_HOME"
+	envUserCache         = "XDG_CACHE_HOME"
+	envUserProgramData   = "XDG_DATA_HOME"
+	envGlobalProgramData = "XDG_DATA_DIRS"
+	envDesktop           = "XDG_DESKTOP_DIR"
+	envDocuments         = "XDG_DOCUMENTS_DIR"
+	envDownloads         = "XDG_DOWNLOAD_DIR"
+	envPictures          = "XDG_PICTURES_DIR"
+	envMusic             = "XDG_MUSIC_DIR"
+	envVideos            = "XDG_VIDEOS_DIR"
+	envSaveGames         = "XDG_DATA_HOME"
 
 	// The environment variables must not be set. Defaults have to be hard coded.
-	CONFIG_LOCAL_DEF  = "${HOME}/.config/"
-	CONFIG_SHARED_DEF = "/etc/xdg/"
-	CACHE_DEF         = "${home}/.cache/"
-	DATA_LOCAL_DEF    = "${home]/.local/share/"
-	DATA_SHARED_DEF   = "/usr/local/share/:/usr/share/"
+	defUserConfig        = "$HOME/.config"
+	defGlobalConfig      = "/etc/xdg"
+	defUserCache         = "$HOME/.cache"
+	defGlobalCache       = "/var/cache"
+	defUserProgramData   = "$HOME/.local/share"
+	defGlobalProgramData = "/usr/share"
+	defDesktop           = "$HOME/Desktop"
+	defDocuments         = "$HOME/Documents"
+	defDownloads         = "$HOME/Downloads"
+	defPictures          = "$HOME/Pictures"
+	defMusic             = "$HOME/Music"
+	defVideos            = "$HOME/Videos"
+	defSaveGames         = "$HOME/.local/share"
 
-	PATH_SEPARATOR = ":"
+	pathSeparator = ":"
 )
 
-var (
-	home      string
-	desktop   string
-	documents string
-	downloads string
-	pictures  string
-	music     string
-	videos    string
-	saveGames string
+func create() Environ {
+	home := getenv(envHome)
+	userConfig := getenvWithDef(envUserConfig, defUserConfig)
+	globalConfig := splitPaths(getenvWithDef(envGlobalConfig, defGlobalConfig))
+	userTemp := getenv(envUserTemp)
+	userCache := getenvWithDef(envUserCache, defUserCache)
+	globalCache := defGlobalCache
+	userProgramData := getenvWithDef(envUserProgramData, defUserProgramData)
+	globalProgramData := splitPaths(getenvWithDef(envGlobalProgramData, defGlobalProgramData))
+	desktop := getenvWithDef(envDesktop, defDesktop)
+	documents := getenvWithDef(envDocuments, defDocuments)
+	downloads := getenvWithDef(envDownloads, defDownloads)
+	pictures := getenvWithDef(envPictures, defPictures)
+	music := getenvWithDef(envMusic, defMusic)
+	videos := getenvWithDef(envVideos, defVideos)
+	saveGames := getenvWithDef(envSaveGames, defSaveGames)
 
-	userConfig        string
-	globalConfig      []string
-	userTemp          string
-	userCache         string
-	globalCache       string
-	localProgramData  string
-	globalProgramData []string
+	return Environ{
+		home:      home,
+		desktop:   desktop,
+		documents: documents,
+		downloads: downloads,
+		pictures:  pictures,
+		music:     music,
+		videos:    videos,
+		saveGames: saveGames,
 
-	configGlobal []string
-	configLocal  string
-	cache        string
-	dataLocal    string
-	dataGlobal   []string
-)
-
-func init() {
-	configLocal = os.Getenv(CONFIG_LOCAL)
-	if os.Getenv(configLocal) == "" {
-		configLocal = os.ExpandEnv(CONFIG_LOCAL_DEF)
+		userConfig:        userConfig,
+		globalConfig:      globalConfig,
+		userTemp:          userTemp,
+		userCache:         userCache,
+		globalCache:       globalCache,
+		userProgramData:   userProgramData,
+		globalProgramData: globalProgramData,
 	}
+}
 
-	var configGlobalTmp = os.Getenv(CONFIG_SHARED)
-	if configGlobalTmp == "" {
-		configGlobalTmp = CONFIG_SHARED_DEF
-	}
-	configGlobal = strings.Split(configGlobalTmp, PATH_SEPARATOR)
+func splitPaths(variable string) []string {
+	return strings.Split(variable, pathSeparator)
+}
 
-	cache = os.Getenv(CACHE)
-	if os.Getenv(configLocal) == "" {
-		cache = os.ExpandEnv(CACHE_DEF)
+func getenv(variable string) string {
+	return os.Getenv(variable)
+}
+func getenvWithDef(variable string, def string) string {
+	path := os.Getenv(variable)
+	if path == "" {
+		return os.ExpandEnv(def)
+	} else {
+		return path
 	}
-
-	dataLocal = os.Getenv(DATA_LOCAL)
-	if os.Getenv(configLocal) == "" {
-		dataLocal = os.ExpandEnv(DATA_LOCAL_DEF)
-	}
-
-	var dataGlobalTmp = os.Getenv(DATA_SHARED)
-	if dataGlobalTmp == "" {
-		dataGlobalTmp = DATA_SHARED_DEF
-	}
-	dataGlobal = strings.Split(dataGlobalTmp, PATH_SEPARATOR)
 }
